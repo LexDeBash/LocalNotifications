55b12013fc4dbc7677c8a5704c05b208eab9d378
Debash <lex.efimov@gmail.com>
2018-06-07 12:51:21 +0500

    Start project

---
+++ LocalNotofications/TableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TableViewController.swift
+//  LocalNotification
+//
+//  Created by Debash on 05.06.2018.
+//  Copyright © 2018 swiftbook.ru. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let notifications = ["Local Notification",
+                         "Local Notification with Action",
+                         "Local Notification with Content",
+                         "Push Notification with  APNs",
+                         "Push Notification with Firebase",
+                         "Push Notification with Content"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifications.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = notifications[indexPath.row]
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let notificationType = notifications[indexPath.row]
+        
+        let alert = UIAlertController(title: "",
+                                      message: "After 5 seconds " + notificationType + " will appear",
+                                      preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
